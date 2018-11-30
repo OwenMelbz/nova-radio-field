a94e930cce9baa2fd8aace175c7d3d90976f3dd5
Owen Melbourne <OwenMelbz@users.noreply.github.com>
2018-11-30 10:27:49 +0000

    Update FormField.vue

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -6,7 +6,7 @@
                 <div v-for="(option, val) in field.options" :class="{'mb-2' : field.stack || field.addPadding}"  class="mlbz-radio-container">
                     <label :for="`${field.attribute}_${val}`">
                         <input v-model="value" :value="val" :id="`${field.attribute}_${val}`" :name="field.attribute" type="radio">
-                        <span class="mblz-radio-label">{{ getOptionLabel(option) }}</span>
+                        <span class="mlbz-radio-label">{{ getOptionLabel(option) }}</span>
                         <span v-if="field.stack && hasOptionHint(option)" class="mlbz-radio-hint mt-1 block text-sm text-80 leading-normal">{{ getOptionHint(option) }}</span>
                     </label>
                 </div>
